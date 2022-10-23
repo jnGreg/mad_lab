683070925228f49ecc940c761d05882950f2207c
jnGreg <janpgregor@gmail.com>
2022-10-23 11:37:52 +0200

    prepared workspace for lab 2

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 UE 22/23 winter
 
 Author: J G
+
+lab 2
+
+segmentation of car models using the [...] grouping methods
+
+Charts:
+
+![Scree plot](scree.jpg)
+
+![Silhouette plot](silhouette.jpg)
+
+Optimal number of crayons: <br>
+K=7 <br>
+![7](7.jpg)
+<br>
+K=10<br>
+![10](10.jpg)
+<br>
+Original picture:<br>
+![Pencils](pencils.jpg)
---
+++ main.py
@@ -0,0 +1,30 @@
+
+import numpy as np
+import pandas as pd
+from plotnine import *
+
+from scipy import stats
+
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+
+from sklearn.mixture import GaussianMixture
+
+from sklearn.metrics import silhouette_score
+
+from sklearn.datasets import make_classification
+# from sklearn.datasets import make_blobs
+# from sklearn.datasets import make_moons
+# from sklearn.datasets import make_circles
+
+from sklearn.preprocessing import StandardScaler
+
+""" https://github.com/jnGreg/mad_lab/tree/2 """
+
+
+def main():
+    print("Hello")
+
+
+if __name__ == "__main__":
+    main()
